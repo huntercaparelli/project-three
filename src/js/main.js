a90d883d2235d48ab0c982cf39ad0d3174368d4d
Hunter Caparelli <hcaparelli@unc.edu>
2022-04-20 12:29:13 -0400

    project-3

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
+var firstName = "Hunter";
+var lastName = "Caparelli";
+var age = 19;
+var favoriteFood = "Lobster";
 
+console.log(
+  `My name is ${firstName}. I am ${age} years old and I love to eat ${favoriteFood}.`
+);
+
+var num = 2;
+var result = num + num;
+
+console.log(result);
+
+var fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+var accordions = document.querySelectorAll(".accordion");
+
+function toggleAccordion() {
+  this.classList.toggle("active");
+}
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", toggleAccordion);
+});
